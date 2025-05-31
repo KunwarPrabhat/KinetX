23bcf261c7db2329ecddfe4f3e236788ca9bda0c
Raj Kunwar Prabhat <rajkunwarprabhat@gmail.com>
2025-06-01 02:16:57 +0530

    starting

---
+++ KinetX/FlatVector.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinetX
+{
+    public readonly struct FlatVector
+    {
+        public readonly float x;
+        public readonly float y;
+    }
+}
